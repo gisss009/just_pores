5a1f64d93e951ed4eafb95c2014cef398fe3a8f1
gisss009 <viktoryagorbaneva@yandex.ru>
2026-05-27 14:55:39 +0300

    add modicated draw

--- drawing_p.py
+++ drawing_p.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 def draw_matrix(file_path):
@@ -15,9 +16,25 @@ def draw_matrix(file_path):
     plt.show()
 
 
+def draw_matrix1(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        matrix = np.array([
+            [int(char) for char in line.strip()]
+            for line in f
+            if line.strip()
+        ])
+
+    print(matrix.shape)
+
+    plt.imshow(matrix, cmap="gray_r")
+    plt.axis("off")
+    plt.show()
+
+
 if __name__ == '__main__':
-    for i in range(0, 16):
-        path = rf'C:\Users\userd\Desktop\Новая папка\just_pores\dataset_pore\sample{i}\output_matrix.txt'
-        draw_matrix(
+    for i in range(1, 10):
+        n = random.randint(1, 1501)
+        path = rf'C:\Users\userd\Desktop\Новая папка\just_pores\update_dataset_pore\sample{n}.txt'
+        draw_matrix1(
             path
         )
